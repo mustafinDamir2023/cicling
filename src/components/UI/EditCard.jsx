73d5d86aa8128d8b40cd858662228d3ba922a866
mustafindamir1985 <mustafind@yandex.ru>
2023-10-13 11:06:00 +0300

    map3

--- src/components/UI/EditCard.jsx
+++ src/components/UI/EditCard.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import { Form, Button, InputGroup } from 'react-bootstrap';
 
-export default function EditCard() {
+export default function EditCard({ rout, submitHandler }) {
+  // const [rout, setRout] = React.useState(route);
+  // const submitHandler = async (e) => {
+  //   e.preventDefault();
+  //   const formData = Object.fromEntries(new FormData(e.target));
+  //   const response = await axios.put(`/api/edit/${route.id}`, formData);
+  //   setRout(response.data);
+  //   console.log('=============', formData);
+  // };
   return (
-    <Form>
+    <Form onSubmit={submitHandler}>
       <InputGroup className="mb-1">
         <InputGroup.Text id="basic-addon1">картинка</InputGroup.Text>
-        <Form.Control
-          placeholder="Username"
-          aria-label="Username"
-          aria-describedby="basic-addon1"
-        />
+        <Form.Control name="img" aria-label="Username" aria-describedby="basic-addon1" />
       </InputGroup>
       <InputGroup className="mb-1">
         <InputGroup.Text id="basic-addon1">название</InputGroup.Text>
         <Form.Control
-          placeholder="Username"
+          name="name"
+          defaultValue={rout.name}
           aria-label="Username"
           aria-describedby="basic-addon1"
         />
@@ -23,7 +28,8 @@ export default function EditCard() {
       <InputGroup className="mb-1">
         <InputGroup.Text id="basic-addon1">город</InputGroup.Text>
         <Form.Control
-          placeholder="Username"
+          name="location"
+          defaultValue={rout.location}
           aria-label="Username"
           aria-describedby="basic-addon1"
         />
@@ -31,14 +37,20 @@ export default function EditCard() {
       <InputGroup className="mb-1">
         <InputGroup.Text id="basic-addon1">дистанция</InputGroup.Text>
         <Form.Control
-          placeholder="Username"
+          name="distance"
+          defaultValue={rout.distance}
           aria-label="Username"
           aria-describedby="basic-addon1"
         />
       </InputGroup>
       <br />
       <div className="d-flex justify-content-center">
-        <Button className="d-flex justify-content-center" variant="secondary" size="sm">
+        <Button
+          className="d-flex justify-content-center"
+          type="submit"
+          variant="secondary"
+          size="sm"
+        >
           редактировать
         </Button>
       </div>
